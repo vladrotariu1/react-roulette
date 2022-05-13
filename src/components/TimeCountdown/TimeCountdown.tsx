49627895541e0e1d0a4c72310c5b48cbf9d1f6d3
Vlad Rotariu <vladrotariu24@gmail.com>
2022-05-13 19:40:38 +0300

    countdown has been synchronized

--- src/components/TimeCountdown/TimeCountdown.tsx
+++ src/components/TimeCountdown/TimeCountdown.tsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import { TimeCountdownProps } from "../../models";
+import { useGetNextRoll } from "../../services/roulette.service";
 import { COUNTDOWN_TIME, Modes } from "../../utils/constants";
 import './TimeCountdown.css'
 
 function TimeCountdown(props: TimeCountdownProps) {
+    const { data: nextRollTimeObject } = useQuery('next-roll', useGetNextRoll(), { enabled: props.shouldRollOrCountDown === Modes.MODE_COUNT_DOWN });
 
     const [timeData, setTimeData] = useState(COUNTDOWN_TIME);
 
     useEffect(() => {
         if (props.shouldRollOrCountDown === Modes.MODE_COUNT_DOWN) {
-            resetState();
+            resetTime();
 
             const countDownInterval = setInterval(() => {
                 updateRemainingTime();
@@ -17,30 +20,39 @@ function TimeCountdown(props: TimeCountdownProps) {
 
             return () => clearInterval(countDownInterval);
         }
-    }, [props.shouldRollOrCountDown]);
+    }, [nextRollTimeObject]);
 
     function updateRemainingTime() {
         setTimeData(time => {
             if (time === 0)
                 props.setModeToRoll();
 
-            return time === 0 ? 0 : time - 1;
+            return time <= 0 ? 0 : time - 1;
         });
     }
 
-    function resetState() {
-        setTimeData(COUNTDOWN_TIME);
+    function resetTime() {
+        const nextRollTime = nextRollTimeObject?.nextRoll;
+
+        if (nextRollTime)
+            setTimeData(Math.floor(nextRollTime));
     }
 
     return (
-        <div
-            style={{ 
-                visibility: 
-                (props.shouldRollOrCountDown === Modes.MODE_ROLL ? 'hidden' : 'visible') 
-            }} 
-            className="time-countdown">
-            Next spin in: {timeData}
-        </div>
+        <>
+        {   
+            timeData <= 10 ?
+            <div
+                style={{ 
+                    visibility: 
+                    (props.shouldRollOrCountDown === Modes.MODE_ROLL ? 'hidden' : 'visible') 
+                }} 
+                className="time-countdown">
+                Next spin in: { timeData }
+            </div> :
+            <div className="time-countdown preparing">Preparing</div>
+        }
+        </>
     );
 }
 
